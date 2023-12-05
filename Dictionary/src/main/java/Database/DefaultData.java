f0c6054e5d1a7262dbbdf9fd35036038bdb16a34
AnhTuan <125129895+anhtuan23004@users.noreply.github.com>
2023-12-05 17:14:37 +0700

    'dict'

---
+++ Dictionary/src/main/java/Database/DefaultData.java
@@ -0,0 +1,17 @@
+package Database;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
+import java.sql.SQLException;
+
+public abstract class DefaultData {
+    private static final String DB_URL_Lite = "jdbc:sqlite:src/main/java/Database/dict_t.db";
+    public static HikariDataSource dataSource = null;
+    public DefaultData() {
+        HikariConfig config = new HikariConfig();
+        config.setJdbcUrl(DB_URL_Lite);
+        config.setMaximumPoolSize(20);
+        dataSource = new HikariDataSource(config);
+    }
+}
---
+++ Dictionary/src/main/java/Speech/TextToSpeechOnline.java
@@ -0,0 +1,79 @@
+package Speech;
+
+import com.example.final_dictionary.AutomaticSoundController;
+import java.io.ByteArrayInputStream;
+import java.io.PrintStream;
+
+import com.voicerss.tts.AudioCodec;
+import com.voicerss.tts.AudioFormat;
+import com.voicerss.tts.Languages;
+import com.voicerss.tts.VoiceParameters;
+import com.voicerss.tts.VoiceProvider;
+
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.DataLine;
+import javax.sound.sampled.SourceDataLine;
+
+public class TextToSpeechOnline {
+    public static void startPlaying(byte[] audio) {
+        try {
+            AudioInputStream ais = new AudioInputStream(
+                    new ByteArrayInputStream(audio),
+                    new javax.sound.sampled.AudioFormat(44100, 16, 2, true, false),
+                    audio.length
+            );
+            javax.sound.sampled.AudioFormat format = ais.getFormat();
+            DataLine.Info info = new DataLine.Info(SourceDataLine.class, format);
+            SourceDataLine speaker = (SourceDataLine) AudioSystem.getLine(info);
+            speaker.open(format);
+            speaker.start();
+            int buffer;
+            byte[] data = new byte[4096];
+            while ((buffer = ais.read(data)) != -1) speaker.write(data, 0, buffer);
+            speaker.drain();
+            speaker.close();
+            ais.close();
+        } catch (Exception e) {
+            e.printStackTrace(new PrintStream(System.out));
+        }
+    }
+
+    public static void textToSpeech(String text) throws Exception{
+        VoiceProvider tts = new VoiceProvider("4f17198eb22e43a9bf77e5ee3bf2e5a7");
+
+        VoiceParameters params = new VoiceParameters(text, Languages.English_UnitedStates);
+
+        if(AutomaticSoundController.isMan)
+            params.setVoice("Mike");
+        else
+            params.setVoice("Mary");
+
+        params.setCodec(AudioCodec.WAV);
+        params.setFormat(AudioFormat.Format_44KHZ.AF_44khz_16bit_stereo);
+        params.setBase64(false);
+        params.setSSML(false);
+        params.setRate(0);
+        byte [] voice = tts.speech(params);
+        startPlaying(voice);
+    }
+
+    public static void textToSpeechVie(String text) throws Exception{
+        VoiceProvider tts = new VoiceProvider("4f17198eb22e43a9bf77e5ee3bf2e5a7");
+
+        VoiceParameters params = new VoiceParameters(text, Languages.Vietnamese);
+
+        if(AutomaticSoundController.isMan)
+            params.setVoice("Mike");
+        else
+            params.setVoice("Mary");
+
+        params.setCodec(AudioCodec.WAV);
+        params.setFormat(AudioFormat.Format_44KHZ.AF_44khz_16bit_stereo);
+        params.setBase64(false);
+        params.setSSML(false);
+        params.setRate(0);
+        byte [] voice = tts.speech(params);
+        startPlaying(voice);
+    }
+}
